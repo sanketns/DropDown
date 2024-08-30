712dddd44a39f01db2a35f51f5b99f00b04ab7a6
sanketns <102017897+sanketns@users.noreply.github.com>
2024-08-30 17:17:37 +0530

    Customize according to requirements

--- DropDown/src/DropDown+Appearance.swift
+++ DropDown/src/DropDown+Appearance.swift
@@ -6,30 +6,26 @@
 //  Copyright © 2016 Kevin Hirsch. All rights reserved.
 //
 
-#if os(iOS)
-
 import UIKit
 
 extension DropDown {
 
-	public class func setupDefaultAppearance() {
-		let appearance = DropDown.appearance()
+    public class func setupDefaultAppearance() {
+        let appearance = DropDown.appearance()
 
-		appearance.cellHeight = DPDConstant.UI.RowHeight
-		appearance.backgroundColor = DPDConstant.UI.BackgroundColor
-		appearance.selectionBackgroundColor = DPDConstant.UI.SelectionBackgroundColor
-		appearance.separatorColor = DPDConstant.UI.SeparatorColor
-		appearance.cornerRadius = DPDConstant.UI.CornerRadius
-		appearance.shadowColor = DPDConstant.UI.Shadow.Color
-		appearance.shadowOffset = DPDConstant.UI.Shadow.Offset
-		appearance.shadowOpacity = DPDConstant.UI.Shadow.Opacity
-		appearance.shadowRadius = DPDConstant.UI.Shadow.Radius
-		appearance.animationduration = DPDConstant.Animation.Duration
-		appearance.textColor = DPDConstant.UI.TextColor
+        appearance.cellHeight = DPDConstant.UI.RowHeight
+        appearance.backgroundColor = DPDConstant.UI.BackgroundColor
+        appearance.selectionBackgroundColor = DPDConstant.UI.SelectionBackgroundColor
+        appearance.separatorColor = DPDConstant.UI.SeparatorColor
+        appearance.cornerRadius = DPDConstant.UI.CornerRadius
+        appearance.shadowColor = DPDConstant.UI.Shadow.Color
+        appearance.shadowOffset = DPDConstant.UI.Shadow.Offset
+        appearance.shadowOpacity = DPDConstant.UI.Shadow.Opacity
+        appearance.shadowRadius = DPDConstant.UI.Shadow.Radius
+        appearance.animationduration = DPDConstant.Animation.Duration
+        appearance.textColor = DPDConstant.UI.TextColor
         appearance.selectedTextColor = DPDConstant.UI.SelectedTextColor
-		appearance.textFont = DPDConstant.UI.TextFont
-	}
+        appearance.textFont = DPDConstant.UI.TextFont
+    }
 
 }
-
-#endif
